d872e2c06042bd53a5f42a47cbae81bccf1f412a
黃柏澄(風雨) <wartw@users.noreply.github.com>
2021-10-07 01:16:03 +0800

    Update default-configuration.js

--- src/widget/default-configuration.js
+++ src/widget/default-configuration.js
@@ -1,19 +1,21 @@
 
 export const defaultConfiguration = {
-    titleClosed: 'Click to chat!',
-    titleOpen: 'Let\'s chat!',
+    titleClosed: '立即諮詢!',
+    titleOpen: 'SYM線上專人客服!',
     closedStyle: 'chat', // button or chat
     closedChatAvatarUrl: '', // only used if closedStyle is set to 'chat'
     cookieExpiration: 1, // in days. Once opened, closed chat title will be shown as button (when closedStyle is set to 'chat')
-    introMessage: 'Hello! How can we help you?',
-    autoResponse: 'Looking for the first available admin (It might take a minute)',
-    autoNoResponse: 'It seems that no one is available to answer right now. Please tell us how we can ' +
-    'contact you, and we will get back to you as soon as we can.',
-    placeholderText: 'Send a message...',
+    introMessage: 'HI!有什麼可以幫你的?'+
+    '(現有用戶請提供帳號與即時PIN碼)',
+    autoResponse: '請稍後線上客服非24小時(Telegram客服同步)',
+    autoNoResponse: '現在沒有客服在線!' +
+    '如購買前問題或是請求測試IP請留下姓名與EMAIL/手機號碼',
+    placeholderText: '請輸入文字...',
     displayMessageTime: true,
     displayBanner: true,
     mainColor: '#1f8ceb',
     alwaysUseFloatingButton: false,
     desktopHeight: 500,
-    desktopWidth: 370
+    desktopWidth: 370,
+    visitorPronoun: 'You'
 };
